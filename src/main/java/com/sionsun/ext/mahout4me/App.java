942c026f060868715b32b45a553394a2c75b8926
yongliangz <yongliangz@oupeng.com>
2015-08-27 14:36:35 +0800

    nothing

--- src/main/java/com/sionsun/ext/mahout4me/App.java
+++ src/main/java/com/sionsun/ext/mahout4me/App.java
@@ -1,13 +1,20 @@
 package com.sionsun.ext.mahout4me;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 
 import org.apache.mahout.cf.taste.common.TasteException;
 import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
 import org.apache.mahout.cf.taste.impl.neighborhood.NearestNUserNeighborhood;
+import org.apache.mahout.cf.taste.impl.recommender.CachingRecommender;
 import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
+import org.apache.mahout.cf.taste.impl.recommender.svd.SVDRecommender;
 import org.apache.mahout.cf.taste.impl.similarity.EuclideanDistanceSimilarity;
 import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
 import org.apache.mahout.cf.taste.model.DataModel;
@@ -20,30 +27,21 @@ import org.apache.mahout.cf.taste.similarity.UserSimilarity;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-    	
-    	
-        System.out.println( "Hello World!" );
+public class App {
+	static final String inputFile = "/home/hadoop-mahout/Downloads/ml-1m/ratings.dat";
+	static final String outputFile = "/home/hadoop-mahout/Downloads/ml-1m/ratings.csv";
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
 		try {
-			DataModel model = new FileDataModel(new File("E:\\workspace_mahout\\mahout4me\\target\\classes\\data.txt"));
-			
-	        UserSimilarity similarity = new EuclideanDistanceSimilarity(model);
-	        
-	        
-	        UserNeighborhood neighborhood = new NearestNUserNeighborhood(2, similarity, model);
-	        
-	        
-	        Recommender recommender = new GenericUserBasedRecommender(model, neighborhood, similarity);
-	        
-	        
-	        
-	        List<RecommendedItem> recommendations = recommender.recommend(3, 2);
-	        for (RecommendedItem recommendation : recommendations) {
-	        	 System.out.println(recommendation);
-	        }
+			createCsvRatingsFile();
+			DataModel model = new FileDataModel(new File(outputFile));
+			SVDRecommender slopeOneRecommender = new SVDRecommender(model, null);
+			CachingRecommender cachingRecommender = new CachingRecommender(slopeOneRecommender);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -52,5 +50,28 @@ public class App
 			e.printStackTrace();
 		}
 
-    }
+	}
+
+	/*
+	 * 
+	 * */
+	private static void createCsvRatingsFile() throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new FileReader(inputFile));
+		BufferedWriter bw = new BufferedWriter(new FileWriter(outputFile));
+		String line = null;
+		String line2write = null;
+
+		String[] temp;
+		int i = 0;
+		while ((line = br.readLine()) != null && i < 10000) {
+			i++;
+			temp = line.split("::");
+			line2write = temp[0] + "," + temp[1];
+			bw.write(line2write);
+			bw.flush();
+		}
+		br.close();
+		bw.close();
+	}
 }
